4588cede24fcdeff55b850715ac04a9a087d776c
sdlfkorg <sdlfkorg@gmail.com>
2018-04-07 21:22:32 +0800

    get a list of accounts from ganache-cli

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,25 +6,43 @@ const Web3 = require('Web3');
 
 const web3 = new Web3(ganache.provider());
 
-class Car{
-	park() {
-		return 'stop';
-	}
-
-}
-
 beforeEach(() => {
-	console.log('before each');
-})
+	// get a list of ganache accounts
+	web3.eth.getAccounts().then(fetchedAccounts => {
+		console.log(fetchedAccounts);
+	});
 
-describe('Car test', () => {
-	it('park!', () => {
-		const car = new Car();
-		assert.equal(car.park(), 'stop');
-	})
-	it('test test', () => {
-		assert.equal(1, 1);
-	})
-
+	// use one of the accounts to deploy contract
 
 });
+
+describe('Inbox', () => {
+	it('deploy a contract', () => {
+
+	});
+});
+
+
+
+// class Car{
+// 	park() {
+// 		return 'stop';
+// 	}
+
+// }
+
+// beforeEach(() => {
+// 	console.log('before each');
+// })
+
+// describe('Car test', () => {
+// 	it('park!', () => {
+// 		const car = new Car();
+// 		assert.equal(car.park(), 'stop');
+// 	})
+// 	it('test test', () => {
+// 		assert.equal(1, 1);
+// 	})
+
+
+// });
