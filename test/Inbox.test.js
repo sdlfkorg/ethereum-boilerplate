e585018e5fccca8672110d60d821b4605d936310
sdlfkorg <sdlfkorg@gmail.com>
2018-04-08 17:00:18 +0800

    test: test the deployed contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,22 +4,37 @@ const ganache = require('ganache-cli');
 // Web3 is constructor func, so W is capital
 const Web3 = require('Web3');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+// const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile.js')
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
 	// get a list of ganache accounts
 	accounts = await web3.eth.getAccounts();
 
 	// use one of the accounts to deploy contract
+	inbox = await new web3.eth.Contract(JSON.parse(interface)).
+		deploy({data: bytecode, arguments: ['hi there!']})
+		.send({from: accounts[0], gas: '1000000'})
+
+	inbox.setProvider(provider);
 
 });
 
 describe('Inbox', () => {
 	it('deploy a contract', () => {
-		console.log(accounts);
+		assert.ok(inbox.options.address);
+		// console.log(inbox);
 	});
+
+	it('has a default message', async () => {
+		const message = await inbox.methods.message().call();
+		assert.equal(message, 'hi there!')
+	})
 });
 
 
